86e274fb2f1893e9b9ae47f0393757b9b5441672
Christopher Suh <ChrisSuh@ChristophersMacbookPro.local>
2017-03-31 23:25:47 -0400

    stuff

--- web/server.js
+++ web/server.js
@@ -19,7 +19,7 @@ db.once('open', function(){
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8000;        // set our port
+var port = process.env.PORT || 8080;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
@@ -27,7 +27,7 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    res.json({ message: 'hooray! welcome to our api!' });
 });
 
 // more routes for our API will happen here
